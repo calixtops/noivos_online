f530924692260fc9db8a8209ca812650707e77f7
Pedro Calixto <calixtops@gmail.com>
2025-08-06 14:57:33 -0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,22 +5,14 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        {/* Favicon animado e estático */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        
-        {/* Meta tags para tema de casamento */}
-        <meta name="theme-color" content="#FF6B9D" />
-        <meta name="msapplication-TileColor" content="#FF6B9D" />
+        {/* Favicon PNG simples */}
+        <link rel="icon" href="/images/auxiliares/favicon.png" type="image/png" />
         
         {/* Preconnect para performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         
-        {/* Google Fonts - Fontes clássicas e elegantes com preload */}
+        {/* Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Source+Sans+Pro:wght@300;400;600&family=Dancing+Script:wght@400;500&family=Cormorant+Garamond:wght@300;400;500;600;700&family=Crimson+Text:wght@400;600&family=Libre+Baskerville:wght@400;700&display=swap"
           rel="stylesheet"
